a0595a0e5b8eddd2e600ffa99cdfa39300c93704
Jc Abalos <jc4balos@gmail.com>
2025-09-08 10:07:49 +0800

    nnagklase sa 2D

--- IT2D/Main.java
+++ IT2D/Main.java
@@ -1,19 +1,17 @@
 package IT2D;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scn1 = new Scanner(System.in);
-        System.out.print("Number of Students");
-        int studentNo = Integer.parseInt(scn1.nextLine());
+        String color = "rgb";
 
-        String[] names = new String[studentNo];
-        Scanner scn = new Scanner(System.in);
-
-        for (int i = 0; i < 5; i++) {
-            System.out.print("Name: ");
-            names[i] = scn.nextLine();
+        if (color == "red") {
+            System.out.print("Stop");
+        } else if (color == "yellow") {
+            System.out.print("Ready for Stop");
+        } else if (color == "green") {
+            System.out.print("GOOOOOOOOOOOOO");
+        } else {
+            System.out.print("Invalid Color");
         }
 
     }
---
+++ IT2D/python/main.py
@@ -0,0 +1,20 @@
+"""
+This is a python program
+This python program has no error :)
+"""
+
+age = 23
+# age = "23"
+print(type(age))
+
+# color = "green"
+
+# if color == "red" and color == "green" and color == "yellow":
+#     print("Valid Traffic Light Color")
+# else:
+#     print("Invalid Traffic Light Color")   
+
+i = 1
+while i <= 10:
+    print(i)
+    i = i + 1
