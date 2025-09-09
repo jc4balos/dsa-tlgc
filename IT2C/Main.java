57406694a046e0d6901074087c2b41ebd827a82d
Jc Abalos <jc4balos@gmail.com>
2025-09-09 08:13:37 +0800

    Create Main.java

---
+++ IT2C/Main.java
@@ -0,0 +1,36 @@
+package IT2C;
+
+public class Main {
+    public static void main(String[] args) {
+        // int age = 20;
+        // if (age < 18) {
+        // System.out.println("Minor");
+        // } else {
+        // System.out.print("Legal");
+        // }
+
+        // int i = 1;
+        // while (i <= 10) {
+        // System.out.print(i);
+        // i++;
+        // }
+
+        // for (int i = 1; i <= 10; i++) {
+        // System.out.println(i);
+        // }
+
+        // int[] ages = new int[10];
+        // ages[6] = 75;
+        // ages[3] = 69;
+        // ages[8] = 14;
+        // ages[0] = 98;
+
+        // for (int x = 0; x <= 9; x++) {
+        // System.out.println(ages[x]);
+        // }
+
+        String[] names = { "Helicopter", "Joel", "Parrot", "Penguin", "Buknoy", "Moana", "Tado" };
+        System.out.println(names[0]);
+    }
+
+}
