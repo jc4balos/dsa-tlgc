ea270dc7c0f7db88ce50362e6f9bf51cde296f6d
Jc Abalos <jc4balos@gmail.com>
2025-09-10 10:31:06 +0800

    nagdisccuss sa IT2A

---
+++ IT2A/App.java
@@ -0,0 +1,27 @@
+package IT2A;
+
+public class App {
+
+    public static void main(String[] args) {
+        // Datatype variableName = value;
+        int age = 32;
+        // age = "32";
+        String name = "Christian";
+        // name = 33;
+        float carSpeed = 32.5f;
+        /*
+         * 
+         * System.out.println(speed);
+         */
+
+        // if (10 < 2) {
+        // System.out.println("10 is greater than 2");
+        // } else {
+        // System.out.println("10 is less than 2");
+        // }
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
