e8d0fc897bf316c6b61533a29b0eddd61bc3d943
Jc Abalos <jc4balos@gmail.com>
2025-09-10 11:48:25 +0800

    oop hehehe

---
+++ IT2A/oop/Car.java
@@ -0,0 +1,13 @@
+package IT2A.oop;
+
+class Car {
+
+    float speed = 65.0f;
+    String color = "pink";
+
+    void start() {
+        System.out.println("The color" + color + " car is starting!");
+
+    }
+
+}
---
+++ IT2A/oop/Main.java
@@ -0,0 +1,18 @@
+package IT2A.oop;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // int x = 3;
+
+        // System.out.print(x);
+        Car x = new Car();// Object of car is created
+        x.start();// called the method start()c
+        x.start();
+        x.start();
+        x.start();
+        x.start();
+
+    }
+
+}
