496c04387eb083710f5e80433f4971086a970750
Jc Abalos <jc4balos@gmail.com>
2025-09-11 13:06:56 +0800

    Create App.java

---
+++ IT2F/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) {
+        String color = "red";
+
+        if (color == "red") {
+            System.out.println("STAPH");
+        } else if (color == "green") {
+            System.out.println("GOOOOOOOOO");
+        } else if (color == "Yellow") {
+            System.out.println("Get ready to stop!!!");
+        } else {
+            System.out.println("Invalid Color");
+        }
+    }
+}
