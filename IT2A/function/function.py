3239a7ce3826b1c21a4e4c1fff328d3217a55b65
Jc Abalos <jc4balos@gmail.com>
2025-09-16 10:30:36 +0800

    discussion on it2a

---
+++ IT2A/function/function.py
@@ -0,0 +1,19 @@
+def solve(x):
+    result = (3*x)+1
+    return result
+
+def bark():
+    print("Arf Arf")
+
+def say(name,age):
+    print("My name is ", name, ". I am ", age, " years old.")
+
+def output(n,word):
+    i = 1
+    while i <= n:# 6<=5 = TRUE
+        print(word)
+        i+=1
+
+output(5,"I love you")
+
+
