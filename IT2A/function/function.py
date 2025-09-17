71ab1e544f73a6c887ce67a30e933a0370502872
Jc Abalos <jc4balos@gmail.com>
2025-09-17 09:46:14 +0800

    discussion on it2a

--- IT2A/function/function.py
+++ IT2A/function/function.py
@@ -2,18 +2,24 @@ def solve(x):
     result = (3*x)+1
     return result
 
+print(solve(1000))
+
 def bark():
     print("Arf Arf")
 
 def say(name,age):
     print("My name is ", name, ". I am ", age, " years old.")
 
-def output(n,word):
+def output(n):
     i = 1
-    while i <= n:# 6<=5 = TRUE
-        print(word)
+    while i <= n:# 6<=5 = FALSE
+        j = 1
+        while  j <=n:
+            print("Loop inside")
+            j+=1
+        print("Loop Outside")
         i+=1
 
-output(5,"I love you")
+output(3)
 
 
