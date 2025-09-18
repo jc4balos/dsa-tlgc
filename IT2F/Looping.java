a24e0a9616cedf6efd6b0e0a67c800c19a046240
Jc Abalos <jc4balos@gmail.com>
2025-09-18 11:00:29 +0800

    nagdiscuss sa IT2F

---
+++ IT2F/Looping.java
@@ -0,0 +1,11 @@
+
+public class Looping {
+    public static void main(String[] args) {
+        // Prin 1-10
+        int x = 1;
+        while (/* condition */x <= 10) {
+            System.out.println(x);
+            x++;
+        }
+    }
+}
---
+++ IT2F/function.py
@@ -0,0 +1,29 @@
+
+# def add(num1,num2):
+#     x = num1+num2
+#     print(x)
+
+# def subtract(num1,num2):
+#     x = num1-num2
+#     print(x)
+
+# subtract(6,5)
+    
+
+def loop(n):
+    i = 1
+    while i <= n:
+        print(i)
+        i+=1
+
+def quadratic_loop(n):
+    i = 1
+    while i <= n:
+        j = 1
+        while j <=n:
+            print("Inner Loop: ", j)
+            j+=1
+        print("Outer Loop", i)
+        i+=1
+
+quadratic_loop(1000) 
